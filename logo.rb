a07fa0078e96c19ac171871b0ca71f6b8441a93d
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-03-25 09:19:32 +0900

    import from jp.rubyist.net

---
+++ logo.rb
@@ -0,0 +1,9 @@
+# -*- coding: euc-jp -*-
+
+def rubynokai
+  %q|<a href="http://jp.rubyist.net/"><img class="rubynokai" src="http://jp.rubyist.net/rubynokai01.png" alt="日本 Ruby の会" width="88" height="31"></a>|
+end
+
+def rubima_logo
+  %q|<a href="./"><img class="rubima_logo" src="rubima_logo.png" alt="るびま" width="135" height="82"></a>|
+end
