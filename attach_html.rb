ba98aa563a02c86865ccddb19c33cd5428830a00
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-05-03 07:51:52 +0900

    fix ArgumentError (invalid byte sequence in UTF-8)

--- attach_html.rb
+++ attach_html.rb
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 def attach_html(file_name, start_no=1, page=@page)
   tabstop = ' ' * (@options['attach.tabstop'] ? @options['attach.tabstop'].to_i : 2)
 
   file = "#{@cache_path}/attach/#{page.untaint.escape}/#{file_name.untaint.escape}"
-  code = File::readlines(file).join.gsub(/^\t+/) {|t| tabstop * t.size}.to_utf8.sub(/\n\z/,'')
+  code = File.read(file).to_utf8.gsub(/^\t+/) {|t| tabstop * t.size}.sub(/\n\z/,'')
   s = ""
   s << %Q!<pre>!
   line = start_no !=- 1 ? start_no : 1
