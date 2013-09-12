8a660f56062fbd4c6656242df799e4b8340bb170
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-09-12 22:45:48 +0900

    use \A instead of ^

--- social_buttons.rb
+++ social_buttons.rb
@@ -3,7 +3,7 @@ def social_buttons
   <<-_E
   <div class="social-buttons" style="text-align:right">
       <!-- hatena bookmark button -->
-      <a href="http://b.hatena.ne.jp/entry/#{url_with_slash.sub(/^https?:\/\//, '')}?#{@conf.options['page']}" class="hatena-bookmark-button" title="Add this entry to Hatena Bookmark"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="Add this entry to Hatena Bookmark" width="20" height="20" style="border: none;" /></a>
+      <a href="http://b.hatena.ne.jp/entry/#{url_with_slash.sub(/\Ahttps?:\/\//, '')}?#{@conf.options['page']}" class="hatena-bookmark-button" title="Add this entry to Hatena Bookmark"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="Add this entry to Hatena Bookmark" width="20" height="20" style="border: none;" /></a>
       <script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8"></script>
       <!-- like button -->
       <iframe src="http://www.facebook.com/plugins/like.php?href=#{CGI.escape url_with_slash}?#{@conf.options['page']}&amp;layout=button_count&amp;show_faces=true&amp;width=100&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
